0d9a896c52f95e00702c69393306eef3fd82ba0c
Parth Bedarkar <code63genos@gmail.com>
2024-11-07 19:56:38 +0530

    a

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from './components/hero'
+import Feature from './components/feature'
+export default function App() {
+  return (
+    <>
+      <Hero/>
+      <Feature/>
+    </>
+  )
+}
